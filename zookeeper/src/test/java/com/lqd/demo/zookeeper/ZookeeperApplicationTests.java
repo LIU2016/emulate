ae72325911f7775fd0d52164bc94e026adcb4f2f
unknown <liu695775009@163.com>
2018-11-23 11:54:09 +0800

    initial

---
+++ zookeeper/src/test/java/com/lqd/demo/zookeeper/ZookeeperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lqd.demo.zookeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZookeeperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
