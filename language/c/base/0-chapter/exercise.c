aea941efd1d9da2cdb784f3f4df8a31a95684045
dinggege <liu695775009@163.com>
2019-03-03 19:22:14 +0800

    c

---
+++ language/c/base/0-chapter/exercise.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+	
+	/**让用户输入一个圆的半径，然后输出这个圆的面积和周长。
+	double r ,s;
+	scanf("%lf", &r) ;
+	s=3.14159*r*r;
+	printf("s:%lf\n",s) ;**/
+	
+	/**让用户输入一个小写字母，然后输出对应的大写字母。**/
+	char a,A;
+	scanf("%c", &a) ;
+	A=a-32;
+	printf("%c\n",A);
+}
